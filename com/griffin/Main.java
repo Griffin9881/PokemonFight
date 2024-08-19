cda2ee8855f2bc71415c5a4f52a6efb983b194e9
Griffin <lamb.griffin@icloud.com>
2024-08-19 19:35:15 -0400

    fixed scanner problem

--- com/griffin/Main.java
+++ com/griffin/Main.java
@@ -3,6 +3,7 @@ package com.griffin;
 import java.util.Scanner;
 
 import com.griffin.pokemon.Pokemon;
+import com.griffin.pokemon.pokemonSpecies.AllPokemon;
 import com.griffin.pokemon.pokemonSpecies.AllPokemonEnum;
 import com.griffin.pokemon.pokemonSpecies.electricPokemon.Pikachu;
 import com.griffin.pokemon.pokemonSpecies.firePokemon.Charmander;
@@ -10,34 +11,36 @@ import com.griffin.pokemon.pokemonSpecies.firePokemon.Charmander;
 public class Main {
     private static Pokemon pokemon1;
     private static Pokemon pokemon2;
+    private static Scanner scanner = new Scanner(System.in);
+    private static String[] AllPokemonNames = AllPokemon.AllPokemonNames;
             public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("What is the first pokemon?");
         String pokemonAnswer = scanner.nextLine();
         System.out.println("What level do you want your pokemon to be?");
-        int levelAnswer = Integer.parseInt(scanner.nextLine());
-        scanner.close();
+        String levelAnswer = scanner.nextLine();
+        int level = Integer.parseInt(levelAnswer);
 
-        AllPokemonEnum answer = FindPokemon.FindThatPokemon(pokemonAnswer);
+        AllPokemonEnum answer = FindThatPokemon(pokemonAnswer);
         switch (answer) {
             case AllPokemonEnum.CHARMANDER:
-                pokemon1 = new Charmander("Charmander", levelAnswer);
+                pokemon1 = new Charmander("Charmander", level);
                 break;
             case AllPokemonEnum.PIKACHU:
-                pokemon1 = new Pikachu("Pikachu", levelAnswer);
+                pokemon1 = new Pikachu("Pikachu", level);
             default:
                 break;
         }
 
-        scanner = new Scanner(System.in);
+        String pokemonAnswer2 = "";
+        int levelAnswer2 = 0;
         System.out.println("What is the other pokemon?");
-        String pokemonAnswer2 = scanner.nextLine();
+        pokemonAnswer2 = scanner.nextLine();
         System.out.println(pokemonAnswer2);
         System.out.println("What level do you want your pokemon to be?");
-        int levelAnswer2 = Integer.parseInt(scanner.nextLine());
+        levelAnswer2 = Integer.parseInt(scanner.nextLine());
 
-        AllPokemonEnum answer2 = FindPokemon.FindThatPokemon(pokemonAnswer2);
+        AllPokemonEnum answer2 = FindThatPokemon(pokemonAnswer2);
         switch (answer2) {
             case AllPokemonEnum.CHARMANDER:
                 pokemon2 = new Charmander("Charmander", levelAnswer2);
@@ -48,5 +51,36 @@ public class Main {
                 break;
         }
         scanner.close();
+    }
+
+    public static AllPokemonEnum FindThatPokemon(String pokemonAnswer) {
+        AllPokemonEnum answer = trySwitch(pokemonAnswer);
+        while (answer == AllPokemonEnum.FAIL) {
+            System.out.println("That is not a valid Pokemon, please pick a pokemon from this list");
+                for (int i = 0; i < AllPokemonNames.length; i++) {
+                    System.out.print(AllPokemonNames[i] + ", ");
+            }
+            System.out.println();
+
+            pokemonAnswer = scanner.nextLine();
+            answer = trySwitch(pokemonAnswer);
+    }
+    return answer;
+}
+
+public static AllPokemonEnum trySwitch (String pokemonAnswer) {
+        try {
+            AllPokemonEnum pokemon = AllPokemonEnum.valueOf(pokemonAnswer.toUpperCase());
+            switch (pokemon) {
+                case AllPokemonEnum.CHARMANDER:
+                    return AllPokemonEnum.CHARMANDER;
+                case AllPokemonEnum.PIKACHU:
+                    return AllPokemonEnum.PIKACHU;
+                default:
+                    return AllPokemonEnum.FAIL;
+            }} catch (IllegalArgumentException e) {   
+            System.out.println("Invalid Pokemon!");
+        }
+        return AllPokemonEnum.FAIL;
     }
 }
