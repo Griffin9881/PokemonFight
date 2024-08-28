8c35fdd0a7a2df463a79bad7ee09b6e5ab4e43ff
Griffin <lamb.griffin@icloud.com>
2024-08-28 17:28:39 -0400

    got rid of two comments

--- com/griffin/Main.java
+++ com/griffin/Main.java
@@ -10,13 +10,13 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
         public static void main(String[] args) {
         pokemon1 = SelectPokemon.PickPokemon(scanner, 1);
-        // pokemon2 = SelectPokemon.PickPokemon(scanner, 2);
+        pokemon2 = SelectPokemon.PickPokemon(scanner, 2);
         pokemon1 = SelectMoves.pickMoves(scanner, pokemon1);
         System.out.println(pokemon1.getMove1());
         System.out.println(pokemon1.getMove2());
         System.out.println(pokemon1.getMove3());
         System.out.println(pokemon1.getMove4());
-        // pokemon2 = SelectMoves.pickMoves(scanner, pokemon2);
+        pokemon2 = SelectMoves.pickMoves(scanner, pokemon2);
         scanner.close();
     }
 }
