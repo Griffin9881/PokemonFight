c5fe9229cacde105486357046d66aa4e78097f76
griffin <lamb.griffin@icloud.com>
2025-11-10 15:27:13 -0500

    weeeeeeeeeee

--- com/griffin/pokemon/moves/fireMoves/FirePunch.java
+++ com/griffin/pokemon/moves/fireMoves/FirePunch.java
@@ -10,7 +10,6 @@ public class FirePunch extends BaseMove implements Fire {
     
     private static final String NAME = "Fire Punch";
     private static final int BASE_POWER = 75;
-    private static final int MAX_POWER = 90;
     private static final int POWER_POINT = 15;
     private static final double ACCURACY = 100.00;
     private static final Targets TARGET = Targets.ALL_POKEMON;
